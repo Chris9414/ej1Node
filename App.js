ec6175f13b989dd901dc7e59a71ba7b174410d66
Chris9414 <chris.and.9414@gmail.com>
2023-02-22 23:09:41 -0500

    first commit

--- App.js
+++ App.js
@@ -6,10 +6,8 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
 app.listen(8000, () => {
   console.log("Servidor corriendo en el puerto 8000");
 });
+
+
